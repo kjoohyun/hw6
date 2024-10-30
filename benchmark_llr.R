13c6cf731fd2fdfdb7cbaa6fe8ecb7f3cf645db3
Joohyun Kim <jhin9400@gmail.com>
2024-10-30 14:21:12 -0400

    speed-test-1

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,29 +1,30 @@
 install.packages("reshape2", type = "binary")
-getwd()
+install.packages("microbenchmark")
 
 source("llr_functions.R")
 # Try out functions to see if it works on some data:
 library(reshape2) # package that contains "french_fries"
+library(microbenchmark)
+
 data(french_fries)
 french_fries = french_fries[complete.cases(french_fries),]
 z = seq(0, 15, length.out = 100)
 fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 2)
 plot(z, fits)
 
+result <- microbenchmark(
+  llr_result = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 2),
+  times = 10
+)
+
+cat("Execution time for w =", i, ":\n")
+print(result)
+
 # Loop through different omega values
 for (i in 1:10){
   fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = i)
   plot(z, fits, main = paste("llr with ω =", i),  xlab= expression(z), ylab = expression(hat(f)(z)))
 }
-
-install.packages("bench")
-library(bench)
-result <- bench::mark(
-  llr_result = llr(z, x, y, omega)
-)
-
-cat("Execution time:/n")
-print(result)
 
 # We can see that the fit is getting more smoother when we increase the value of omega.
 # In locally weighted regression, the hyperparameter omega controls how much of the 
