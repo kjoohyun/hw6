8bffa7270b002867b9240222d9019a0d54e66313
kjoohyun <jhin9400@gmail.com>
2024-10-30 22:06:05 -0400

    Update llr_functions.R

--- llr_functions.R
+++ llr_functions.R
@@ -8,8 +8,8 @@ llr = function(x, y, z, omega) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
-  WX = apply(X, 2, function(col) col * Wz)
-  Wy = Wz * y
+  WX = sweep(X, 2, Wz[1,], `*`)
+  Wy = Wz %*% y
   f_hat = c(1, z) %*% solve(t(WX) %*% WX) %*% t(WX) %*% Wy
   return(f_hat)
 }
